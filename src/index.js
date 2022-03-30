724ca1dbecfc2f065c1deed1db4559adbdf48389
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-30 16:28:35 -0400

    clicking sun/moon toggles theme. Video starry background, TEST COLORS ONLY atm, nice!

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+// set default lightmode, and mode-change script
+window.onload = function () {
+  const body = document.querySelector("body");
+  const theme_btn = document.getElementById("toggle-dark-mode");
+
+  theme_btn.onclick = function () {
+    body.classList.toggle("dark");
+  };
+};
